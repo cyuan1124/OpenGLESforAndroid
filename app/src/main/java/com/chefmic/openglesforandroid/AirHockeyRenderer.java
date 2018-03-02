6c50b0a3287e43884c65db76050fe3e77b4fe4ae
Chen Yuan <cyuan1124@users.noreply.github.com>
2018-03-01 23:10:33 -0500

    Finish Chapter 5

--- app/src/main/java/com/chefmic/openglesforandroid/AirHockeyRenderer.java
+++ app/src/main/java/com/chefmic/openglesforandroid/AirHockeyRenderer.java
@@ -1,10 +1,16 @@
+/***
+ * Excerpted from "OpenGL ES for Android",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
+ ***/
 package com.chefmic.openglesforandroid;
 
 import android.content.Context;
-import android.opengl.GLSurfaceView;
-import android.support.annotation.NonNull;
+import android.opengl.GLSurfaceView.Renderer;
 
-import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -22,98 +28,171 @@ import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.orthoM;
 
-public class AirHockeyRenderer implements GLSurfaceView.Renderer {
-
+public class AirHockeyRenderer implements Renderer {
+    private static final String U_MATRIX = "u_Matrix";
     private static final String A_POSITION = "a_Position";
     private static final String A_COLOR = "a_Color";
-
-    private static final int BYTES_PER_FLOAT = 4;
     private static final int POSITION_COMPONENT_COUNT = 2;
-
     private static final int COLOR_COMPONENT_COUNT = 3;
-    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
-
-    private final WeakReference<Context> context;
     private final FloatBuffer vertexData;
+    private final Context context;
+    private final float[] projectionMatrix = new float[16];
+
+    private int program;
+    private int uMatrixLocation;
     private int aPositionLocation;
     private int aColorLocation;
-    private int program;
 
-    public AirHockeyRenderer(@NonNull Context context) {
-        this.context = new WeakReference<>(context);
-
+    public AirHockeyRenderer(Context context) {
+        this.context = context;
+        /*
         float[] tableVerticesWithTriangles = {
-                // X, Y, R, G, B
-                // Triangle Fan
-                0, 0, 1f, 1f, 1f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            // Triangle Fan
+               0,     0,
+            -0.5f, -0.5f,
+             0.5f, -0.5f,
+             0.5f,  0.5f,
+            -0.5f,  0.5f,
+            -0.5f, -0.5f,
 
-                -0.5f, 0f, 1f, 0f, 1f,
+            // Line 1
+            -0.5f, 0f,
+             0.5f, 0f,
+
+            // Mallets
+            0f, -0.25f,
+            0f,  0.25f
+        };*/
+
+        //
+        // Vertex data is stored in the following manner:
+        //
+        // The first two numbers are part of the position: X, Y
+        // The next three numbers are part of the color: R, G, B
+        //
+        float[] tableVerticesWithTriangles = {
+                // Order of coordinates: X, Y, R, G, B
+
+                // Triangle Fan
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+
+                // Line 1
+                -0.5f, 0f, 1f, 0f, 0f,
                 0.5f, 0f, 1f, 0f, 0f,
 
-                0f, -0.25f, 0f, 0f, 1f,
-                0f, 0.25f, 1f, 0f, 0f
+                // Mallets
+                0f, -0.4f, 0f, 0f, 1f,
+                0f, 0.4f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
                 .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
 
         vertexData.put(tableVerticesWithTriangles);
     }
 
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        glClearColor(0f, 0.0f, 0.0f, 0.0f);
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-        String vertexShaderSource = TextResourceReader.readTextFileFromResource(context.get(), R.raw.simple_vertex_shader);
-        String fragmentShaderSource = TextResourceReader.readTextFileFromResource(context.get(), R.raw.simple_fragment_shader);
+        String vertexShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        String fragmentShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
 
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
-        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        int fragmentShader = ShaderHelper
+                .compileFragmentShader(fragmentShaderSource);
 
         program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
         ShaderHelper.validateProgram(program);
 
         glUseProgram(program);
 
-        aColorLocation = glGetAttribLocation(program, A_COLOR);
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
 
+        // Bind our data, specified by the variable vertexData, to the vertex
+        // attribute at location A_POSITION_LOCATION.
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
+
         glEnableVertexAttribArray(aPositionLocation);
 
+        // Bind our data, specified by the variable vertexData, to the vertex
+        // attribute at location A_COLOR_LOCATION.
         vertexData.position(POSITION_COMPONENT_COUNT);
-        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
+
         glEnableVertexAttribArray(aColorLocation);
+
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
     }
 
+    /**
+     * onSurfaceChanged is called whenever the surface has changed. This is
+     * called at least once when the surface is initialized. Keep in mind that
+     * Android normally restarts an Activity on rotation, and in that case, the
+     * renderer will be destroyed and a new one created.
+     *
+     * @param width  The new width, in pixels.
+     * @param height The new height, in pixels.
+     */
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        // Set the OpenGL viewport to fill the entire surface.
+        glViewport(0, 0, width, height);
 
+        final float aspectRatio = width > height ? width / (float) height
+                                                 : height / (float) width;
+
+        if (width > height) {
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
 
+    /**
+     * OnDrawFrame is called whenever a new frame needs to be drawn. Normally,
+     * this is done at the refresh rate of the screen.
+     */
     @Override
-    public void onDrawFrame(GL10 gl) {
+    public void onDrawFrame(GL10 glUnused) {
+        // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
+
+        // Draw the table.
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
-        // Draw line
+        // Draw the center dividing line.
         glDrawArrays(GL_LINES, 6, 2);
 
-        // Draw mallet
+        // Draw the first mallet.
         glDrawArrays(GL_POINTS, 8, 1);
 
+        // Draw the second mallet.
         glDrawArrays(GL_POINTS, 9, 1);
     }
 }
